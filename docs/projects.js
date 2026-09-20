3280114aeff172c479991f97c97ea753f33f0cb3
DimiS0 <24132721@student.hhs.nl>
2026-09-20 22:49:56 +0200

    Update portfolio project page and styles

---
+++ docs/projects.js
@@ -0,0 +1,47 @@
+const projecten = [
+    {
+    titel: "Hotel simulator",
+    beschrijving: "simulating a real life hotel with events",
+    afbeelding: "image.png",
+    alt: "Een screenshot van een 2d hotel layout met gasten en schoonmakers in beeld",
+    link: "https://github.com/DimiS0/Hotel-simulatie---groep-7-",
+    kaartKlasse: "project-card1",
+    imageKlasse: "project-image",
+    descKlasse: "project-description"
+},
+{
+     titel: "Recycling app",
+    beschrijving: "an application for our trashcan that scans plastic with an API and opens it, if its correct",
+    afbeelding: "image2.png",
+    alt: "Een screenshot van een log-in scherm van een prullen bak project",
+    link: "https://github.com/DimiS0/Coderen3",
+    kaartKlasse: "project-card2",
+    imageKlasse: "project-image2",
+    descKlasse: "project-description2"
+}
+]
+
+const grid = document.getElementById("project-card-grid");
+
+const ProjectenLatenZien = (lijst) => {
+    grid.innerHTML = "";
+
+
+    lijst.forEach(projecten => {
+        const artiekel = document.createElement("Article");
+        artiekel.classList.add(projecten.kaartKlasse);
+
+        artiekel.innerHTML = `<div class = "${projecten.imageKlasse}"> 
+                            <img src = "${projecten.afbeelding}"alt="${projecten.alt}"></img>
+                            </div>
+                            <div class = "${projecten.descKlasse}">
+                            <h3>${projecten.titel}</h3>
+                            <p>${projecten.beschrijving}</p>
+                            <a href = "${projecten.link}"><strong>View project</strong></a>
+                            </div>
+                            `;   
+       grid.appendChild(artiekel);                       
+    });
+}
+
+ProjectenLatenZien(projecten);
